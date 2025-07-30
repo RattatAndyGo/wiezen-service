92b77fda6428dc34a880e520bc3d1df7f2d3b32c
RattatAndyGo <andries66deklerck@gmail.com>
2025-07-30 16:57:54 +0200

    dummy search for testing

--- web.py
+++ web.py
@@ -1,6 +1,5 @@
 # see https://github.com/mu-semtech/mu-python-template for more info
 
-@app.route("/hello")
-def hello():
-    return "Hello from the mu-python-template!"
-
+@app.route("/search/<playername>", methods=["GET"])
+def search(playername):
+    return "Searched for {}".format(playername)
