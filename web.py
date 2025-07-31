513d8fef8cbabbc79ade1f441c8aa0e11ce47015
RattatAndyGo <andries66deklerck@gmail.com>
2025-07-31 22:12:08 +0200

    made retrieving old games from triplestore functional

--- web.py
+++ web.py
@@ -7,48 +7,95 @@ from string import Template
 
 @app.route("/search/<playername>", methods=["GET"])
 def search(playername):
-    result = query(Template("""
+    result = {}
+
+    # Get all rounds the player participated in
+    round_query = query(Template("""
         PREFIX schema: <http://schema.org/>
-        PREFIX ext: <http://example.com/>
-        SELECT * WHERE { 
+        SELECT DISTINCT ?round WHERE { 
             ?namedPlayer schema:givenName $name.
             ?namedPos schema:competitor ?namedPlayer.
             ?round schema:event ?namedPos.
-            ?hand schema:superEvent ?round.
-            ?hand schema:description ?contract.
-            ?hand ext:index ?index.
-            ?hand schema:referee ?dealer.
-            ?dealer schema:givenName ?dealerName.
-            ?hand ext:gameScore ?point.
-            ?point ext:value ?score.
-            ?point ext:winner ?winner.
-            ?winner schema:competitor ?player.
-            ?player schema:givenName ?name.
         } LIMIT 100
     """).substitute(name=sparql_escape_string(playername)))
 
-    # return result
+    rounds = []
 
-    return """[
-            {
-                "dealer":"A",
-                "contract":"Vraag mee",
-                "activePlayers":[true, true, false, true],
-                "pointsEarned":["2","2","-2","-2"],
-                "pointsTotal":["2","2","-2","-2"]
-            },
-            {
-                "dealer":"B",
-                "contract":"Abondance 9",
-                "activePlayers":[true, true, false, false],
-                "pointsEarned":["-5","-5","15","-5"],
-                "pointsTotal":[-3,-3,13,-7]
-            },
-            {
-                "dealer":"C",
-                "contract":"Alleen",
-                "activePlayers":[true, true, false, false],
-                "pointsEarned":["-12","4","4","4"],
-                "pointsTotal":[-15,1,17,-3]
+    for roundbinding in round_query["results"]["bindings"]:
+        round = {}
+        round_uri = roundbinding["round"]["value"]
+
+        # Get all the players from the specific round
+        player_query = query(Template("""
+            PREFIX schema: <http://schema.org/>
+            PREFIX ext: <http://example.com/>
+            SELECT DISTINCT ?name WHERE { 
+                $round schema:event ?pos.
+                ?pos schema:competitor ?player.
+                ?player schema:givenName ?name.
+                ?pos ext:value ?index.
+            } ORDER BY ?index
+        """).substitute(round=sparql_escape_uri(round_uri)))
+        playernames = [playerbinding["name"]["value"] for playerbinding in player_query["results"]["bindings"]]
+        round["players"] = playernames
+
+        # Get all the hands from the specific route
+        hand_query = query(Template("""
+            PREFIX schema: <http://schema.org/>
+            PREFIX ext: <http://example.com/>
+            SELECT DISTINCT ?hand ?contract ?dealerName WHERE { 
+                ?hand schema:superEvent $round.
+                ?hand schema:description ?contract.
+                ?hand ext:index ?index.
+                ?hand schema:referee ?dealer.
+                ?dealer schema:givenName ?dealerName.
+            } ORDER BY ?index
+        """).substitute(round=sparql_escape_uri(round_uri)))
+
+        # List for calculation cumulative scores
+        points_total = [0 for _ in range(4)]
+        games = []
+
+        for handbinding in hand_query["results"]["bindings"]:
+            game = {}
+            hand_uri = handbinding["hand"]["value"]
+
+            # Get the scores from the specific hand
+            point_query = query(Template("""
+                PREFIX ext: <http://example.com/>
+                SELECT DISTINCT ?point ?score WHERE { 
+                    $hand ext:gameScore ?point.
+                    ?point ext:value ?score.
+                    ?point ext:winner ?position.
+                    ?position ext:value ?index.
+                } ORDER BY ?index
+            """).substitute(hand=sparql_escape_uri(hand_uri)))
+
+            points_earned = [int(pointbinding["score"]["value"]) for pointbinding in point_query["results"]["bindings"]]
+            points_total = [points_earned[i] + points_total[i] for i in range(4)]
+
+            active_players_query = query(Template("""
+                PREFIX schema: <http://schema.org/>
+                SELECT DISTINCT ?name WHERE { 
+                    $hand schema:homeTeam ?player.
+                    ?player schema:givenName ?name.
+                }
+            """).substitute(hand=sparql_escape_uri(hand_uri)))
+
+            active_playernames = [playerbinding["name"]["value"] for playerbinding in active_players_query["results"]["bindings"]]
+            active_players = [playername for playername in playernames if playername in active_playernames]             
+
+            game = {
+                "dealer": handbinding["dealerName"]["value"],
+                "contract": handbinding["contract"]["value"],
+                "activePlayers": active_players,
+                "pointsEarned": points_earned,
+                "pointsTotal": points_total
             }
-        ]"""
+
+            games.append(game)
+        round["games"] = games
+        rounds.append(round)
+    result["rounds"] = rounds
+    
+    return jsonify(result)
